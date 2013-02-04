1b3bcb38e8008684a4f57303bc61efbb96388388
Robert Basic <robertbasic.com@gmail.com>
2013-02-04 20:50:19 +0100

    initials. doing the same via where and pattern matching

--- guards.hs
+++ guards.hs
@@ -15,3 +15,12 @@ calculateBmi' weight height
     | otherwise = "Damn."
     where h = (if height > 100 then height / 100 else height)
           bmi = weight / h ^ 2
+
+
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+    where (f:_) = firstname
+          (l:_) = lastname
+
+initials' :: String -> String -> String
+initials' (f:firstname) (l:lastname) = [f] ++ ". " ++ [l] ++ "."
