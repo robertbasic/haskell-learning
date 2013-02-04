637521870b7e74fe8837db81937879d58659a834
Robert Basic <robertbasic.com@gmail.com>
2013-02-04 20:32:44 +0100

    where for guards

--- guards.hs
+++ guards.hs
@@ -1,7 +1,17 @@
 -- these | guards are like if/else structures
-calculateBmi :: Float -> Float -> String
+calculateBmi :: (RealFloat a) => a -> a -> String
 calculateBmi weight height
     | weight / height ^ 2 <= 18.5 = "Skinny."
     | weight / height ^ 2 <= 25.0 = "Normal."
     | weight / height ^ 2 < 30 = "Stop eating."
     | otherwise = "Damn."
+
+-- indentation for where needs to be correct
+calculateBmi' :: (RealFloat a) => a -> a -> String
+calculateBmi' weight height
+    | bmi <= 18.5 = "Skinny."
+    | bmi <= 25.0 = "Normal."
+    | bmi < 30 = "Stop eating."
+    | otherwise = "Damn."
+    where h = (if height > 100 then height / 100 else height)
+          bmi = weight / h ^ 2
